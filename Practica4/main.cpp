e889d90ed6539c3bfa6b1c121a5cb110b1e3f766
andresLafo <andreslafaurie777@gmail.com>
2025-11-01 11:24:18 -0500

    se me olvido hacer commit ayer xd

--- Practica4/main.cpp
+++ Practica4/main.cpp
@@ -1,19 +1,193 @@
-#include <QCoreApplication>
+#include <iostream>
+#include "Red.h"
+#include "Enrutador.h"
 
-int main(int argc, char *argv[])
-{
-    QCoreApplication a(argc, argv);
+using namespace std;
 
-    // Set up code that uses the Qt event loop here.
-    // Call a.quit() or a.exit() to quit the application.
-    // A not very useful example would be including
-    // #include <QTimer>
-    // near the top of the file and calling
-    // QTimer::singleShot(5000, &a, &QCoreApplication::quit);
-    // which quits the application after 5 seconds.
+Red redGlobal;
 
-    // If you do not need a running Qt event loop, remove the call
-    // to a.exec() or use the Non-Qt Plain C++ Application template.
+bool pedirEnrutadores(char *nombres) {
+    bool existe = true;
 
-    return a.exec();
+    for (int i = 0; i < 2; i++) {
+        cout << "Ingrese el nombre del enrutador " << (i + 1) << ": ";
+        cin >> nombres[i];
+
+        if (!redGlobal.existeEnrutador(nombres[i])) {
+            cout << "El enrutador '" << nombres[i] << "' no existe en la red." << endl;
+            existe = false;
+            break;
+        }
+    }
+
+    return existe;
+}
+
+// ------------------------------------------------------------
+// Programa principal
+// ------------------------------------------------------------
+int main() {
+    int opcion = 0;
+
+    do {
+        cout << "\n==================== MENU PRINCIPAL ====================\n";
+        cout << "1. Agregar enrutador\n";
+        cout << "2. Eliminar enrutador\n";
+        cout << "3. Ver enrutadores y vecinos\n";
+        cout << "4. Ver tablas de enrutamiento\n";
+        cout << "5. Agregar relacion (enlace)\n";
+        cout << "6. Ver costo entre dos enrutadores\n";
+        cout << "7. Ver ruta mas corta entre dos enrutadores\n";
+        cout << "8. Cargar red desde archivo\n";
+        cout << "9. Cambiar costo de un enlace\n";
+        cout << "10. Generar red aleatoria\n";
+        cout << "0. Salir\n";
+        cout << "=========================================================\n";
+        cout << "Seleccione una opcion: ";
+        cin >> opcion;
+        cout << endl;
+
+        switch (opcion) {
+        case 1: {
+            char nombre;
+            cout << "Ingrese el nombre del nuevo enrutador: ";
+            cin >> nombre;
+
+            if (redGlobal.existeEnrutador(nombre)) {
+                cout << "El enrutador " << nombre << " ya existe.\n";
+            } else {
+                redGlobal.agregarEnrutador(nombre);
+                cout << "Enrutador '" << nombre << "' agregado correctamente.\n";
+            }
+            break;
+        }
+
+        case 2: {
+            char nombre;
+            cout << "Ingrese el nombre del enrutador a eliminar: ";
+            cin >> nombre;
+
+            if (redGlobal.existeEnrutador(nombre)) {
+                redGlobal.eliminarEnrutador(nombre);
+                cout << "Enrutador '" << nombre << "' eliminado correctamente.\n";
+            } else {
+                cout << "El enrutador '" << nombre << "' no existe.\n";
+            }
+            break;
+        }
+
+        case 3: { // Ver enrutadores
+            redGlobal.verRed();
+            break;
+        }
+
+        case 4: { // Ver tablas de enrutamiento
+            redGlobal.verTablas();
+            break;
+        }
+
+        case 5: { // Agregar relación
+            char nombres[2];
+            if (!pedirEnrutadores(nombres)) break;
+
+            int costo;
+            cout << "Ingrese el costo del enlace entre " << nombres[0] << " y " << nombres[1] << ": ";
+            cin >> costo;
+
+            if (costo > 0) {
+                redGlobal.conectarEnrutadores(nombres[0], nombres[1], costo);
+                cout << "Relacion agregada correctamente.\n";
+            } else {
+                cout << "El costo debe ser mayor que 0.\n";
+            }
+            break;
+        }
+
+        case 6: { // Ver costo entre dos routers
+            char nombres[2];
+            if (!pedirEnrutadores(nombres)) break;
+
+            int costo = redGlobal.obtenerCosto(nombres[0], nombres[1]);
+            if (costo == -1)
+                cout << "No hay conexion entre " << nombres[0] << " y " << nombres[1] << ".\n";
+            else
+                cout << "Costo total de " << nombres[0] << " a " << nombres[1] << ": " << costo << endl;
+            break;
+        }
+
+        case 7: { // Ver ruta más corta
+            char nombres[2];
+            if (!pedirEnrutadores(nombres)) break;
+
+            string camino = "";
+            redGlobal.obtenerCaminoMasCorto(nombres[0], nombres[1], camino);
+
+            if (camino.empty())
+                cout << "No hay ruta disponible entre esos enrutadores.\n";
+            else
+                cout << "La ruta mas corta de " << nombres[0] << " a " << nombres[1] << " es: "
+                     << nombres[0] << " -> " << camino << endl;
+            break;
+        }
+
+        case 8: { // Cargar red desde archivo
+            string archivo;
+            cout << "Ingrese el nombre del archivo (con extension .txt): ";
+            cin >> archivo;
+
+            int resultado = redGlobal.cargarDesdeArchivo(archivo);
+            if (resultado == 1)
+                cout << "Red cargada exitosamente desde el archivo.\n";
+            else
+                cout << "Error al intentar cargar el archivo.\n";
+            break;
+        }
+
+        case 9: { // Cambiar costo de un enlace
+            char nombres[2];
+            if (!pedirEnrutadores(nombres)) break;
+
+            int nuevoCosto;
+            cout << "Ingrese el nuevo costo para la conexion entre " << nombres[0] << " y " << nombres[1] << ": ";
+            cin >> nuevoCosto;
+
+            if (nuevoCosto <= 0) {
+                cout << "El costo debe ser mayor que 0.\n";
+            } else {
+                Enrutador A = redGlobal.obtenerEnrutador(nombres[0]);
+                Enrutador B = redGlobal.obtenerEnrutador(nombres[1]);
+
+                if (!A.existeVecino(nombres[1])) {
+                    cout << "Los enrutadores no estan conectados.\n";
+                } else {
+                    A.actualizarCostoVecino(nombres[1], nuevoCosto);
+                    B.actualizarCostoVecino(nombres[0], nuevoCosto);
+
+                    redGlobal.actualizarEnrutador(A);
+                    redGlobal.actualizarEnrutador(B);
+                    redGlobal.crearTablas();
+
+                    cout << "Costo actualizado correctamente.\n";
+                }
+            }
+            break;
+        }
+
+        case 10: { // Generar red aleatoria
+            redGlobal.generarAleatoria();
+            break;
+        }
+
+        case 0:
+            cout << "Saliendo del programa...\n";
+            break;
+
+        default:
+            cout << "Opcion no valida. Intente nuevamente.\n";
+            break;
+        }
+
+    } while (opcion != 0);
+
+    return 0;
 }
--- Practica4/red.cpp
+++ Practica4/red.cpp
@@ -1,3 +1,274 @@
-#include "red.h"
+#include "Red.h"
 
-red::red() {}
+Red::Red() {}
+
+void Red::agregarEnrutador(char nombre) {
+
+    Enrutador nuevo(nombre);
+    nuevo.agregarVecino(nombre, 0);
+    this->enrutadores.push_back(nuevo);
+}
+
+bool Red::existeEnrutador(char nombre) {
+    for (auto &r : this->enrutadores) {
+        if (r.obtenerNombre() == nombre)
+            return true;
+    }
+    return false;
+}
+
+Enrutador Red::obtenerEnrutador(char nombre) {
+    for (auto &r : this->enrutadores) {
+        if (r.obtenerNombre() == nombre)
+            return r;
+    }
+    return Enrutador('?');
+}
+
+void Red::actualizarEnrutador(Enrutador obj) {
+    for (auto &r : this->enrutadores) {
+        if (r.obtenerNombre() == obj.obtenerNombre()) {
+            r = obj;
+            break;
+        }
+    }
+}
+
+
+void Red::eliminarEnrutador(char nombre) {
+    for (auto it = this->enrutadores.begin(); it != this->enrutadores.end(); ++it) {
+        if (it->obtenerNombre() == nombre) {
+            this->enrutadores.erase(it);
+            break;
+        }
+    }
+
+    this->actualizarRelaciones(nombre);
+    this->crearTablas();
+}
+
+
+void Red::conectarEnrutadores(char origen, char destino, int costo) {
+    Enrutador A = this->obtenerEnrutador(origen);
+    Enrutador B = this->obtenerEnrutador(destino);
+
+    A.agregarVecino(destino, costo);
+    B.agregarVecino(origen, costo);
+
+    this->actualizarEnrutador(A);
+    this->actualizarEnrutador(B);
+
+    this->crearTablas();
+}
+
+void Red::desconectarEnrutadores(char origen, char destino) {
+    Enrutador A = this->obtenerEnrutador(origen);
+    Enrutador B = this->obtenerEnrutador(destino);
+
+    A.eliminarVecino(destino);
+    B.eliminarVecino(origen);
+
+    this->actualizarEnrutador(A);
+    this->actualizarEnrutador(B);
+
+    this->crearTablas();
+}
+
+
+void Red::actualizarRelaciones(char nombreEliminado) {
+    for (auto &r : this->enrutadores) {
+        if (r.existeVecino(nombreEliminado)) {
+            r.eliminarVecino(nombreEliminado);
+        }
+    }
+}
+
+
+void Red::crearTablas() {
+    vector<char> visitados;
+
+    for (auto &routerOrigen : this->enrutadores) {
+        routerOrigen.limpiarDestinos();
+
+        for (auto &routerDestino : this->enrutadores) {
+            visitados.clear();
+            visitados.push_back(routerOrigen.obtenerNombre());
+
+            map<char, int> resultado = this->calcularCamino(routerOrigen, routerDestino, visitados, 0, 0, true);
+
+            if (resultado.begin()->second == 1000)
+                routerOrigen.definirRuta(resultado.begin()->first, routerDestino.obtenerNombre(), -1);
+            else
+                routerOrigen.definirRuta(resultado.begin()->first, routerDestino.obtenerNombre(), resultado.begin()->second);
+        }
+
+        this->actualizarEnrutador(routerOrigen);
+    }
+}
+
+
+map<char, int> Red::calcularCamino(Enrutador inicio, Enrutador fin, vector<char> visitados, int costoAcum, int anterior, bool primerPaso) {
+    map<char, int> resultado;
+    resultado[inicio.obtenerNombre()] = 1000; // valor grande por defecto
+
+    if (inicio.obtenerNombre() == fin.obtenerNombre()) {
+        resultado[inicio.obtenerNombre()] = 0;
+        return resultado;
+    }
+
+    map<char, int> vecinos = inicio.obtenerVecinosFiltrados(visitados);
+
+    int costoTemporal = 0;
+    for (auto const &v : vecinos) {
+        if (primerPaso && visitados.size() > 1)
+            visitados.pop_back();
+
+        Enrutador siguiente = this->obtenerEnrutador(v.first);
+
+        // Si el vecino es el destino directo
+        if (v.first == fin.obtenerNombre()) {
+            if ((costoTemporal - anterior) > 0)
+                costoAcum = (costoTemporal - anterior);
+
+            costoAcum += (v.second + anterior);
+
+            if (costoAcum < resultado.begin()->second) {
+                resultado.clear();
+                resultado[v.first] = costoAcum;
+            }
+
+            costoAcum = 0;
+        }
+        else {
+            visitados.push_back(v.first);
+            costoAcum += anterior;
+
+            map<char, int> subRuta = this->calcularCamino(siguiente, fin, visitados, costoAcum, v.second, false);
+
+            if (subRuta.begin()->second < resultado.begin()->second) {
+                resultado.clear();
+                resultado[v.first] = subRuta.begin()->second;
+            }
+
+            costoTemporal = costoAcum;
+            costoAcum = 0;
+        }
+    }
+
+    return resultado;
+}
+
+void Red::verRed() {
+    cout << endl << "La red contiene " << this->enrutadores.size() << " enrutador(es)." << endl;
+
+    for (auto &r : this->enrutadores) {
+        cout << endl << "Enrutador " << r.obtenerNombre() << ":" << endl;
+
+        map<char, int> vecinos = r.obtenerVecinosFiltrados({});
+        for (auto const &v : vecinos) {
+            cout << "   - " << v.first << " con costo de: " << v.second << endl;
+        }
+    }
+}
+
+void Red::verTablas() {
+    for (auto &r : this->enrutadores) {
+        cout << endl << "Tabla de enrutamiento de " << r.obtenerNombre() << ":" << endl;
+
+        map<char, map<char, int>> tabla = r.obtenerDestinos();
+
+        for (auto const &fila : tabla) {
+            cout << "| Destino " << fila.first << ": ";
+
+            map<char, int> entrada = fila.second;
+            if (entrada.begin()->second == -1)
+                cout << "sin ruta";
+            else
+                cout << "siguiente salto " << entrada.begin()->first << ", costo total " << entrada.begin()->second;
+
+            cout << endl;
+        }
+    }
+}
+
+
+int Red::obtenerCosto(char origen, char destino) {
+    Enrutador r = this->obtenerEnrutador(origen);
+    map<char, map<char, int>> tabla = r.obtenerDestinos();
+
+    for (auto const &fila : tabla) {
+        if (fila.first == destino)
+            return fila.second.begin()->second;
+    }
+
+    return -1;
+}
+
+
+void Red::obtenerCaminoMasCorto(char origen, char destino, string &camino) {
+    Enrutador r = this->obtenerEnrutador(origen);
+    map<char, map<char, int>> tabla = r.obtenerDestinos();
+
+    for (auto const &fila : tabla) {
+        if (fila.first == destino) {
+            char siguiente = fila.second.begin()->first;
+            camino += siguiente;
+
+            if (siguiente == destino)
+                break;
+
+            obtenerCaminoMasCorto(siguiente, destino, camino);
+        }
+    }
+}
+
+int Red::cargarDesdeArchivo(string nombreArchivo) {
+    ifstream archivo("C:/Users/Andres Felipe/OneDrive/Escritorio/informatica II Andres Lafaurie/INFORMATICA 2/Practica4/Practica4/red1.txt");
+    if (!archivo.is_open()) {
+        cout << "Error: no se pudo abrir el archivo." << endl;
+        return -1;
+    }
+
+    char origen, destino;
+    int costo;
+
+    while (archivo >> origen >> destino >> costo) {
+        if (!this->existeEnrutador(origen))
+            this->agregarEnrutador(origen);
+        if (!this->existeEnrutador(destino))
+            this->agregarEnrutador(destino);
+
+        this->conectarEnrutadores(origen, destino, costo);
+    }
+
+    archivo.close();
+    cout << "Archivo cargado correctamente." << endl;
+    return 1;
+}
+
+void Red::generarAleatoria() {
+    string letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    srand(time(NULL));
+
+    int cantidad = 2 + rand() % 9; // entre 2 y 10 routers
+    vector<char> nombres;
+
+    for (int i = 0; i < cantidad; ++i) {
+        char nombre = letras[i];
+        this->agregarEnrutador(nombre);
+        nombres.push_back(nombre);
+    }
+
+    int conexiones = cantidad + rand() % (cantidad * 2);
+
+    for (int i = 0; i < conexiones; ++i) {
+        char a = nombres[rand() % cantidad];
+        char b = nombres[rand() % cantidad];
+        if (a == b) continue;
+        int costo = 1 + rand() % 19;
+
+        this->conectarEnrutadores(a, b, costo);
+    }
+
+    cout << "Red aleatoria generada con " << cantidad << " enrutadores." << endl;
+}
